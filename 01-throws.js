47b86d085a2e84b47ff4a636c4a106f05bbf1078
sinjin25 <raionus2@gmail.com>
2022-04-01 17:07:29 -0700

    improve 01-throws test readability

--- 01-throws.js
+++ 01-throws.js
@@ -1,32 +1,49 @@
-function myTest() {
-    return Promise.resolve(15)
+require('colors')
+
+function myTest(id) {
+    return Promise.resolve(id)
 }
 
-// then followed by catch
-// mytest resolved 15
-// then it threw Error: An error
-myTest()
-.then((result) => {
-    console.log('mytest resolved', result)
-    throw Error('An error')
-})
-.catch((err) => {
-    console.log('Then it threw', err)
-})
+async function main() {
+    // then followed by catch
+    // mytest resolved 15
+    // then it threw Error: An error
+    await myTest(15)
+    .then((result) => {
+        console.log('mytest resolved'.green, result)
+        throw Error('some random error')
+    })
+    .catch((err) => {
+        console.log('Then it threw'.red, err)
+    })
 
-// then+catch
-// mytest resolved 15
-// 'Then the throw got handled in the next catch' An error
-myTest()
-.then((result) => {
-    console.log('mytest resolved', result)
-    throw Error('An error')
-}, (err) => {
-    console.log('Will the throw be handled here?', err) // this would only handle if the previous then made an error
-})
-.catch((err) => {
-    console.log('Then the throw got handled in the next catch', err)
-})
+    // then+catch
+    // mytest resolved 15
+    // 'Then the throw got handled in the next catch' An error
+    await myTest(11)
+    .then((result) => {
+        console.log('mytest resolved'.green, result)
+        throw Error('some random error')
+    }, (err) => {
+        console.log('Will the throw be handled here?'.blue, err) // this would only handle if the previous .then made an error
+    })
+    .catch((err) => {
+        console.log('Then the throw got handled in the next catch'.red, err)
+    })
 
-// running these two tests together will result in resolve runnign together, then error runnign together
-// this is because the shit is throw in the event loop (queue); it is not synchronous, even if it resolves instantly
+    await myTest('This will crash the program')
+    .then((result) => {
+        try {
+            console.log('mytest resolved'.green, result)
+            throw Error('some random error')
+        } catch(e) {
+            return Promise.reject(e.red)
+        }
+    }, (err) => {
+        console.log('Will the throw be handled here?'.blue, err) // this would only handle if the previous .then made an error
+    })
+
+    console.log('this line will not run because the above test crashed the program'.blue)
+}
+
+main()
